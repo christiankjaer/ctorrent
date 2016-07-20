d31e5a0b375bdacb9fd97d9e50eeb976315927b4
Christian Kjær Larsen <christianden@gmail.com>
2016-07-20 17:20:37 +0200

    More stuff

--- ctorrent.c
+++ ctorrent.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "bencoding.h"
 #include <curl/curl.h>
+#include "bencoding.h"
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +8,26 @@ int main(int argc, char *argv[])
     bencoding_t* metainfo = read_bencoding(stream);
 
     bencoding_t* ann = find_in_dict(metainfo, "announce");
-    print_bencoding(ann);
+
+    char* url = ann->data.b_string;
+
+    puts(url);
+
+    CURL *curl;
+    CURLcode res;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK)
+          fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+ 
+    /* always cleanup */ 
+    curl_easy_cleanup(curl);
+  }
     return 0;
 }
