f310be6d0e6e80be18977f1692327ea9783c8a60
Christian Kjær Larsen <christianden@gmail.com>
2016-07-20 13:35:01 +0200

    Crude bencoding parser

---
+++ bencoding.c
@@ -0,0 +1,127 @@
+#include <stdlib.h>
+
+#include "bencoding.h"
+
+bencoding_t* read_bencoding(FILE* stream) {
+    char c = fgetc(stream);
+    switch (c) {
+        case 'i': 
+            return read_int(stream);
+        case 'l':
+            return read_list(stream);
+        case 'd':
+            return read_dict(stream);
+        default:
+            ungetc(c, stream);
+            return read_string(stream);
+    }
+}
+bencoding_t* read_string(FILE* stream) {
+    int size;
+    fscanf(stream, "%d:", &size);
+    char* data = (char*)malloc(sizeof(char) * (size+1));
+    fread(data, sizeof(char), size, stream);
+    data[size] = '\0';
+    bencoding_t* ret = (bencoding_t*)malloc(sizeof(bencoding_t));
+    ret->type = B_STRING;
+    ret->data.b_string = data;
+    return ret;
+}
+bencoding_t* read_int(FILE* stream) {
+    int i;
+    fscanf(stream, "%de", &i);
+    bencoding_t* ret = (bencoding_t*)malloc(sizeof(bencoding_t));
+    ret->type = B_INT;
+    ret->data.b_int = i;
+    return ret;
+}
+bencoding_t* read_list(FILE* stream) {
+    bencoding_t* ret = (bencoding_t*)malloc(sizeof(bencoding_t));
+    ret->type = B_LIST;
+    ret->data.b_list = NULL;
+    node_t** pp = &ret->data.b_list;
+    char c;
+    while ((c = fgetc(stream)) != 'e') {
+        ungetc(c, stream);
+        bencoding_t* data = read_bencoding(stream);
+        node_t* elem = (node_t*)malloc(sizeof(node_t));
+        elem->data = data;
+        elem->next = NULL;
+        *pp = elem;
+        pp = &elem->next;
+    }
+    return ret;
+}
+bencoding_t* read_dict(FILE* stream) {
+    bencoding_t* ret = (bencoding_t*)malloc(sizeof(bencoding_t));
+    ret->type = B_DICT;
+    ret->data.b_dict = NULL;
+    dict_t** pp = &ret->data.b_dict;
+    char c;
+    while ((c = fgetc(stream)) != 'e') {
+        ungetc(c, stream);
+        int size;
+        fscanf(stream, "%d:", &size);
+        char* key = (char*)malloc(sizeof(char) * (size+1));
+        fread(key, sizeof(char), size, stream);
+        key[size] = '\0';
+        bencoding_t* data = read_bencoding(stream);
+        dict_t* elem = (dict_t*)malloc(sizeof(dict_t));
+        elem->key = key;
+        elem->data = data;
+        elem->next = NULL;
+        *pp = elem;
+        pp = &elem->next;
+    }
+    return ret;
+}
+
+void print_string(bencoding_t* b) {
+    printf("\"%s\"\n", b->data.b_string);
+}
+void print_int(bencoding_t* b) {
+    printf("%d\n", b->data.b_int);
+}
+void print_list(bencoding_t* b) {
+    node_t* p = b->data.b_list;
+    puts("[");
+    while (p) {
+        print_bencoding(p->data);
+        p = p->next;
+    }
+    puts("]");
+}
+
+void print_dict(bencoding_t* b) {
+    dict_t* p = b->data.b_dict;
+    puts("{");
+    while (p) {
+        printf("%s: ", p->key);
+        print_bencoding(p->data);
+        p = p->next;
+    }
+    puts("}");
+}
+
+void print_bencoding(bencoding_t* b) {
+    switch (b->type) {
+        case B_INT:
+            print_int(b);
+            break;
+        case B_STRING:
+            print_string(b);
+            break;
+        case B_LIST:
+            print_list(b);
+            break;
+        default:
+            print_dict(b);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    FILE* stream = fopen(argv[1], "r");
+    print_bencoding(read_bencoding(stream));
+    return 0;
+}
---
+++ bencoding.h
@@ -0,0 +1,40 @@
+#ifndef BENCODING_H
+#define BENCODING_H
+
+#include <stdio.h>
+
+enum BENC_TYPE { B_STRING, B_INT, B_LIST, B_DICT };
+
+typedef struct dict_t dict_t;
+typedef struct node_t node_t;
+typedef struct bencoding_t bencoding_t;
+
+struct node_t {
+    bencoding_t* data;
+    node_t* next;
+};
+
+struct dict_t {
+    char* key;
+    bencoding_t* data;
+    dict_t* next;
+};
+
+typedef struct bencoding_t {
+    int type;
+    union {
+        char* b_string;
+        int b_int;
+        node_t* b_list;
+        dict_t* b_dict;
+    } data;
+} bencoding_t;
+
+bencoding_t* read_bencoding(FILE*);
+bencoding_t* read_string(FILE*);
+bencoding_t* read_int(FILE*);
+bencoding_t* read_list(FILE*);
+bencoding_t* read_dict(FILE*);
+void print_bencoding(bencoding_t*);
+
+#endif
